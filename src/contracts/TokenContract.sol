bbb380fda658a03cbdfc85d367b21d84a6c68b3a
João Alcaraz <joao.alcaraz@sou.inteli.edu.br>
2023-05-06 20:53:40 -0300

    typo fix

--- src/contracts/TokenContract.sol
+++ src/contracts/TokenContract.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MeuToken is ERC20, Ownable {
-    constructor(uint256 initialSupply) ERC20("GreeToken", "GTK") {
+    constructor(uint256 initialSupply) ERC20("GreenToken", "GTK") {
         _mint(msg.sender, initialSupply);
     }
 
